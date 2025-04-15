ee248b5e5ffcc5837e116ec6e5b6e50e55ffb556
serhaterdogan <serhaterdogan@ankara.edu.tr>
2025-04-15 12:11:10 +0300

    back to chiefcomplaint

--- app_ed.py
+++ app_ed.py
@@ -97,7 +97,7 @@ def load_and_filter_data():
             diagnoses_df = diagnoses_df[diagnoses_df['icd_code'].isin(icd_code_filter)]
 
         if chiefcomplaint_filter and "chiefcomplaint" in patients_df.columns:
-            patients_df = patients_df[patients_df["chiefcomplaint"].astype(str).str.contains(chiefcomplaint_filter, case=False)]
+            patients_df = patients_df[patients_df["chiefcomplaint"].astype(str).str.contains(chiefcomplaint_filter, case=False, na=False)]
 
         merge_keys = ["subject_id"]
         if "hadm_id" in patients_df.columns and "hadm_id" in diagnoses_df.columns:
